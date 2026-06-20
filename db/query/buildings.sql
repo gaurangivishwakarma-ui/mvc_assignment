2bfa6bc11ec72b6a3492f30d932e178747886b56
gaurangi <gaurangivishwakarma@gmail.com>
2026-06-20 18:23:11 +0530

    feat(buildings):implement building move system, along with sql queries and sqlc generated code

---
+++ db/query/buildings.sql
@@ -0,0 +1,10 @@
+-- name: GetOwnedBuildingPositionInfo :one
+SELECT bo.player_id, b.width, b.breadth
+FROM buildings_owned bo
+JOIN buildings b ON bo.building_id = b.id
+WHERE bo.id = $1;
+
+-- name: UpdateBuildingPosition :exec
+UPDATE buildings_owned
+SET x_coords = $2, y_coords = $3
+WHERE id = $1 AND player_id = $4;
